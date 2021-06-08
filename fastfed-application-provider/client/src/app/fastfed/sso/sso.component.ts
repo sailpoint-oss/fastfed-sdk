f0a877a188e63a49ca8482ee7fd5b1e2964e8cdb
Matt Domsch <matt.domsch@sailpoint.com>
2021-06-08 16:19:24 +0000

    use xml-js to display SSO federation info
    Signed-off-by: Matt Domsch <matt.domsch@sailpoint.com>

--- fastfed-application-provider/client/src/app/fastfed/sso/sso.component.ts
+++ fastfed-application-provider/client/src/app/fastfed/sso/sso.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {BehaviorSubject, Observable, Subscription} from 'rxjs';
 import {AppService} from '../../common/services/app.service';
-import { NgxXml2jsonService } from 'ngx-xml2json';
+var convert = require('xml-js');
 
 @Component({
     selector: 'app-sso',
@@ -12,11 +12,9 @@ export class SsoComponent implements OnInit, OnDestroy {
 
     private ssoSettingsSubscription: Subscription;
     private ssoSettingsSubject: BehaviorSubject<any>;
-    private parser: DOMParser;
 
     constructor(private applicationService: AppService) {
         this.ssoSettingsSubject = new BehaviorSubject<any>(null);
-        this.parser = new DOMParser();
     }
 
     getSsoSettings(): Observable<any> {
@@ -24,8 +22,8 @@ export class SsoComponent implements OnInit, OnDestroy {
     }
 
     public toJSON(xml: string): string {
-        const parsedXml = this.parser.parseFromString(xml, 'text/xml');
-        return NgxXml2jsonService.xmlToJson(parsedXml);
+    	var options = {compact: true, spaces: 4};
+        return convert.xml2json(xml, options);
     }
 
 
